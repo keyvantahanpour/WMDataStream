097c06a68e79aed36cca56b215b5246fe54268c4
Keyvan <s>
2025-02-03 20:08:42 +0330

    initial commit

---
+++ src/main/java/com/keyvan/wmdatastream/WmDataStreamApplication.java
@@ -0,0 +1,13 @@
+package com.keyvan.wmdatastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmDataStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmDataStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keyvan/wmdatastream/WmDataStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyvan.wmdatastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmDataStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
